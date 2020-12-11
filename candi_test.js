f4bad0ca9a0ffdb1db77ac0d8b0488ab6688bed3
Reuben <rp506@exeter.ac.uk>
2020-12-11 14:11:07 +0000

    Update candi_test.js

--- candi_test.js
+++ candi_test.js
@@ -1,42 +1,50 @@
 'use strict';
 
 const cheerio = require("../node_modules/cheerio");
-const Knwl = require("../node_modules/knwl.js");
-const knwlInstance = new Knwl("english");
+const Knwl    = require("../node_modules/knwl.js");
 const request = require("../node_modules/request");
+const knwlInstance = new Knwl("english");
 
-const email = "canddi.com";
+const email = "tim@canddi.com";
 const options = {
-    url: "https://www." + email,
+    url: "https://www." + email.slice( email.indexOf('@') + 1 ),
     method: "GET"
 };
 
-
-var links = [];
+var information = {
+    description: "",
+    names:       [],
+    addresses:   [],
+    numbers:     [],
+    emails:      [],
+    links:       []
+};
 var link;
 request(options, function (err, response, html) {
     console.log("err=" + err + ", statusCode=" + response.statusCode)
     if (!err && response.statusCode == 200) {
         var $ = cheerio.load(html);
-        knwlInstance.init($.text());
-        var emails = knwlInstance.get("emails");
-        var places = knwlInstance.get("places");
-        console.log(emails);
-        console.log(places);
+
+        //Start with meta tags
+        $("meta").filter(function () {
+            //Check if name attribute is author and the content attribute exists
+            var name = String($(this).attr("name"));
+            var content = String($(this).attr("content"));
+            console.log(name + ", " + content);
+        });
 
         //Search the links in the footer
-        $("footer").find("a").each(function () {  
+        $("footer").find("a").each(function () {
             if ($(this).attr("href")) {
-                link = String($(this).attr("href"));
-                if (link.startsWith("http") && links.indexOf(link) === -1) {
-                    links.push(link);
+                link = String( $(this).attr("href") );
+                if (link.startsWith("http") && information.links.indexOf(link) === -1) {
+                    information.links.push(link);
                 }
             }
         });
-        console.log(links);
+
+        console.log(information);
     } else {
         console.log("Could not connect to website.");
     }
 });
-
-console.log("end");
