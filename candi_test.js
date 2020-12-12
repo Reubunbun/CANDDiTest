b0a9671067e07ac09cb5cf1610c3e9a6c6a4cdd5
Reuben <rp506@exeter.ac.uk>
2020-12-12 13:27:09 +0000

    Update candi_test.js

--- candi_test.js
+++ candi_test.js
@@ -5,9 +5,10 @@ const Knwl    = require("../node_modules/knwl.js");
 const request = require("../node_modules/request");
 const knwlInstance = new Knwl("english");
 
-const email = "tim@canddi.com";
+const email  = "tim.langley@canddi.com";
+const domain = email.slice(email.indexOf('@') + 1);
 const options = {
-    url: "https://www." + email.slice( email.indexOf('@') + 1 ),
+    url: "https://www." + domain,
     method: "GET"
 };
 
@@ -26,11 +27,13 @@ request(options, function (err, response, html) {
     if (!err && response.statusCode == 200) {
         const $ = cheerio.load(html);
 
+        var name;
+        var content;
         //Start with meta tags
-        $("meta").filter(function () {
+        $("meta").each(function () {
             //Check if name attribute is author and the content attribute exists
-            var name    = String( $(this).attr("name")    );
-            var content = String( $(this).attr("content") );
+            name    = String( $(this).attr("name")    );
+            content = String( $(this).attr("content") );
             if (name === "author") {
                 information.names.push(content);
             } else if (name === "description") {
@@ -38,15 +41,20 @@ request(options, function (err, response, html) {
             }
         });
 
-        $("footer").find('*').each(function () {
-            //Check for links
-            if ($(this).prop("nodeName") === "A" && $(this).attr("href")) {
+        //Check for external links to crawl
+        $("a").each(function () {
+            if ($(this).attr("href")) {
                 link = String($(this).attr("href"));
-                if (link.startsWith("http") && information.links.indexOf(link) === -1) {
+                if (link.startsWith("http") && !link.includes(domain) && information.links.indexOf(link) === -1) {
                     information.links.push(link);
+                    //if (link.indexOf("www.facebook.com") > -1) crawlFacebook(link, information);
                 }
+            }
+        });
+
+        $("footer").find('*').each(function () {
             //Check for numbers/emails/places
-            } else if ($(this).text()) {
+            if ($(this).text()) {
                 checkKnwl($(this).text(), "phones", information, knwlInstance);
                 checkKnwl($(this).text(), "emails", information, knwlInstance);
                 checkKnwl($(this).text(), "places", information, knwlInstance);
@@ -55,10 +63,33 @@ request(options, function (err, response, html) {
 
         console.log(information);
     } else {
-        console.log("Could not connect to website.");
+        if (err) {
+            console.log(err);
+        } else if (response.statusCode != 200) {
+            console.log("HTTP error: " + response.statusCode);
+        }
     }
 });
 
+function crawlFacebook(link, info) {
+    //Crawl facebook page to find company location + phone number
+    //Doesn't work as facebook doesn't allow crawling like this, would be
+    //better to use their api with an access token.
+    const options = {
+        url: link.endsWith("/about/") ? link : link + "/about/",
+        method: "GET",
+        headers: { "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36 Edg/85.0.564.44" }
+    };
+    request(options, function (err, response, html) {
+        console.log("err=" + err + ", statusCode=" + response.statusCode)
+        if (!err && response.statusCode == 200) {
+            const $ = cheerio.load(html);
+            console.log($.text());
+        } else {
+            console.log("Could not connect to website.");
+        }
+    });
+}
 
 function checkKnwl(text, type, info, knwl) {
     //Checks text for type (phones/places/emails), and adds to the information object if found.
