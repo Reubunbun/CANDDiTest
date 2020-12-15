10e3eecbde4d0d3366fce29269a34dd47c53a109
Reuben <rp506@exeter.ac.uk>
2020-12-15 16:28:42 +0000

    Update tests.js

--- tests.js
+++ tests.js
@@ -7,6 +7,9 @@ const pEmails = [
     "l@platform-recruitment.com", "o@twitter.com", "y@linkedin.com", "e@oscar-tech.com"
 ]
 
+const sURLRegEx =
+    /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
+
 let pTotalTime  = [0, 0];
 let iTotalNames = 0;
 let iTotalAddresses = 0;
@@ -23,7 +26,13 @@ function executeTest(sEmail) {
         let pHrEnd = process.hrtime(pHrStart);
         let sTestText = `Result for ${sEmail}, took ${pHrEnd[0]}s ${pHrEnd[1] / 1000000}ms\n${JSON.stringify(oInfo)}\n`;
         sTestText += `Errors: ${oInfo.pError.length}, Names: ${oInfo.pNames.length}, Addresses: ${oInfo.pAddresses.length}, Numbers: ${oInfo.pNumbers.length}`;
-        sTestText += `, Emails: ${oInfo.pEmails.length}, Links: ${oInfo.pLinks.length}\n\n`;
+        sTestText += `, Emails: ${oInfo.pEmails.length}, Links: ${oInfo.pLinks.length}`;
+        let pBadURLs = oInfo.pLinks.filter(link => { link.match(sURLRegEx) });
+        if (pBadURLs.length > 0) {
+            sTestText += pBadURLs.length + "\n incorrect urls found: "
+            pBadURLs.forEach(url => { sTestText += url + ", " });
+        }
+        sTestText += "\n\n";
         console.log("writing: " + sTestText);
         fs.appendFile("results.txt", sTestText, err => { if (err) throw err; });
 
